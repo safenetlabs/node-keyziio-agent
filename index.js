d1adcad481a5994320be25bcabddd444dbf50b4e
David Pitard <david.pitard@safenet-inc.com>
2014-08-07 16:53:27 -0400

    More stuff working

--- index.js
+++ index.js
@@ -2,5 +2,88 @@
  * Created by dpitard on 8/6/14.
  */
 
-module.exports = {}
+var request = require('request');
+var Promise = require('promise');
 
+var token;
+
+module.exports = {
+    set_token: function(in_token) {
+        token = in_token;
+        return true;
+    },
+
+    get_token: function() {
+        return token;
+    },
+
+    check: check,
+
+    get_user: get_user,
+
+    create_user: create_user
+};
+
+function check2(){
+    var promise = new Promise(function(resolve, reject){
+        request("http://safex-demo.herokuapp.com/api/v1/check?api_token=" + token, function(error, resp, body){
+            if (!error && resp.statusCode == 200) {
+                resolve(true)
+            }
+            else {
+                resolve(false)
+            }
+        });
+    });
+    //return promise;
+}
+
+function check(callback) {
+    url = "http://safex-demo.herokuapp.com/api/v1/check?api_token=" + token
+    request.get(url, function (error, resp, body) {
+        if (!error && resp.statusCode == 200) {
+            callback(true)
+        }
+        else {
+            callback(false)
+        }
+    });
+}
+
+function get_user(id, callback) {
+    var url = "http://safex-demo.herokuapp.com/api/v1/users/" + id + "?api_token=" + token;
+    request.get(url,
+        {headers: {
+            'Accept': 'application/json'
+            },
+            json: true
+        },
+        function (error, resp, body){
+        if (!error && resp.statusCode == 200) {
+            callback(body, false)
+        }
+        else {
+            callback(null, true)
+        }
+    });
+}
+
+function create_user(id, friendly_name, callback) {
+    var url = "http://safex-demo.herokuapp.com/api/v1/users?api_token=" + token;
+    request.post(url,
+        {form: {asp_id: id, friendly_name: friendly_name},
+         headers: {
+             'Accept':'application/json'
+         },
+         json: true
+        },
+
+        function(error, resp, body){
+            if (!error && resp.statusCode == 201) {
+                callback(body, false)
+            }
+            else {
+                callback(null, true)
+            }
+    })
+}
--- test/index.js
+++ test/index.js
@@ -1,12 +1,57 @@
 /**
  * Created by dpitard on 8/6/14.
  */
-var should = require('chai').should(),
-    aasguard = require('../index');
 
+require('mocha-sinon');
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+
+var should = require('chai').should();
+var aasguard = require('../index');
 
 describe('#aasguard', function(){
+    var aasguard;
+    beforeEach(function(){
+        aasguard = require('../index');
+        aasguard.set_token("slZxxbJai9hOWyRRYBrMWA").should.equal(true);
+    });
+
     it ('runs a non test', function() {
         true.should.equal(true);
+    });
+
+    it ('sets and gets the token', function() {
+        aasguard.set_token("testtoken").should.equal(true);
+        aasguard.get_token().should.equal('testtoken')
+    });
+
+    it ('checks against the server', function(done) {
+        //aasguard.check().should.eventually.equal(true);
+
+        aasguard.check(function(ok){
+                ok.should.equal(true)
+                done()
+            }
+        )
+    });
+
+    it ('tells me a user is not there', function(done){
+        aasguard.get_user("fakeid", function(user, error){
+            error.should.equal(true);
+            done()
+        })
+    });
+
+    it ('creates a new user', function(done){
+        var uid = Math.random().toString().substring(2);
+        aasguard.create_user(uid, "friendly", function(user, error){
+            error.should.equal(false);
+
+            aasguard.get_user(uid, function(user, error){
+                error.should.equal(false)
+                done()
+            })
+        })
     })
 });
