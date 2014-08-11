0ad5db2249d49c5f5eb41b316ef6347bf186e5f5
David Pitard <david.pitard@safenet-inc.com>
2014-08-11 10:55:07 -0400

    Added mock testing framework

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@
 var chai = require("chai");
 var chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
-
+var nock = require("nock");
 var should = require('chai').should();
 var aasguard = require('../index');
 
@@ -12,11 +12,7 @@ describe('#aasguard', function(){
     var aasguard;
     beforeEach(function(){
         aasguard = require('../index');
-        aasguard.set_token("slZxxbJai9hOWyRRYBrMWA").should.equal(true);
-    });
-
-    it ('runs a non test', function() {
-        true.should.equal(true);
+        aasguard.set_token("cnZNqHoOXDML9eSSZI").should.equal(true);
     });
 
     it ('sets and gets the token', function() {
@@ -25,15 +21,37 @@ describe('#aasguard', function(){
     });
 
     it ('checks against the server', function(done) {
+        var mock = nock('http://safex-demo.herokuapp.com')
+            .log(console.log)
+            .filteringPath(/api_token=[^&]*/g, 'api_token=XXX')
+            .get('/api/v1/check?api_token=XXX')
+            .reply(200, {
+            });
+
         aasguard.check().should.eventually.equal(true).notify(done);
     });
 
     it ('tells me a user is not there', function(done){
+        var mock = nock('http://safex-demo.herokuapp.com')
+            .log(console.log)
+            .filteringPath(/api_token=[^&]*/g, 'api_token=XXX')
+
+            .get('api/v1/users/fakeid?api_token=XXX')
+            .reply(404);
+
         aasguard.get_user("fakeid").should.be.rejected.notify(done);
     });
 
     it ('creates a new user', function(done){
         var uid = Math.random().toString().substring(2);
+        var mock = nock('http://safex-demo.herokuapp.com')
+            .log(console.log)
+            .filteringPath(/api_token=[^&]*/g, 'api_token=XXX')
+            .post('/api/v1/users?api_token=XXX','asp_id=' + uid +'&friendly_name=friendly')
+            .reply(201)
+            .get('/api/v1/users/' + uid + '?api_token=XXX')
+            .reply(200);
+
         aasguard.create_user(uid, "friendly")
             .then(aasguard.get_user(uid))
             .should.eventually.be.fulfilled.notify(done);
